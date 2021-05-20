85b84a0c5a7896fa93823e9e0415e0e474de51aa
DittoGod <DittoGod@users.noreply.github.com>
2021-05-20 17:28:41 +0200

    Updates

---
+++ precache-manifest.6e03435f4282038c24be2cd43700959d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717f43b74034457c60fe0b49a7dc36d7",
+    "url": "/dittogod.github.io/developerFolio/index.html"
+  },
+  {
+    "revision": "63e10101be43efb8476c",
+    "url": "/dittogod.github.io/developerFolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "2000d1c21d477666a8b8",
+    "url": "/dittogod.github.io/developerFolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "dd9fb29b05beb8a5ec63",
+    "url": "/dittogod.github.io/developerFolio/static/css/main.d892e3f3.chunk.css"
+  },
+  {
+    "revision": "e5b396a9977b6c7867ec",
+    "url": "/dittogod.github.io/developerFolio/static/js/2.82bfb1bc.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/dittogod.github.io/developerFolio/static/js/2.82bfb1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e10101be43efb8476c",
+    "url": "/dittogod.github.io/developerFolio/static/js/3.f0863308.chunk.js"
+  },
+  {
+    "revision": "2000d1c21d477666a8b8",
+    "url": "/dittogod.github.io/developerFolio/static/js/4.d7ad8b81.chunk.js"
+  },
+  {
+    "revision": "dd9fb29b05beb8a5ec63",
+    "url": "/dittogod.github.io/developerFolio/static/js/main.ea697c19.chunk.js"
+  },
+  {
+    "revision": "9f9e7a439fa176802041",
+    "url": "/dittogod.github.io/developerFolio/static/js/runtime-main.e4e24f9a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/dittogod.github.io/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/dittogod.github.io/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/dittogod.github.io/developerFolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/dittogod.github.io/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/dittogod.github.io/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/dittogod.github.io/developerFolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/dittogod.github.io/developerFolio/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/dittogod.github.io/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/dittogod.github.io/developerFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/dittogod.github.io/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/dittogod.github.io/developerFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/dittogod.github.io/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/dittogod.github.io/developerFolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/dittogod.github.io/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
