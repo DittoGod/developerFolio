63fc9b257a8a62165205161d822819fb35c409ff
DittoGod <dittogod1@gmail.com>
2022-10-28 19:48:13 +1100

    Updates

--- precache-manifest.9e998cf18653370468fc6c931ca99b6b.js
+++ precache-manifest.9e998cf18653370468fc6c931ca99b6b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a0232d1207c641087ace1b085b77970a",
+    "revision": "5e478e88a62610812bcc76a7b9b0fe97",
     "url": "/index.html"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/4.d7ceb645.chunk.css"
   },
   {
-    "revision": "e12f7e914036c2f7ce93",
+    "revision": "e0624b55805d5d4dfd1b",
     "url": "/static/css/main.b8516e90.chunk.css"
   },
   {
@@ -32,8 +32,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.e5bcfff7.chunk.js"
   },
   {
-    "revision": "e12f7e914036c2f7ce93",
-    "url": "/static/js/main.2ef8085e.chunk.js"
+    "revision": "e0624b55805d5d4dfd1b",
+    "url": "/static/js/main.33dc1b8c.chunk.js"
   },
   {
     "revision": "411c7cb50c44ccedb7e9",
