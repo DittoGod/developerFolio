f4c0ec639718326e34a0fe88e323257c41986422
DittoGod <simba.ndoro@dittogod.com>
2026-06-10 21:04:28 +0100

    fix: remove duplicate resumeSection declaration in portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -441,15 +441,6 @@ const resumeSection = {
   display: true // Set false to hide this section, defaults to true
 };
 
-// Resume Section
-const resumeSection = {
-  title: "Resume",
-  subtitle: "Feel free to download my resume",
-
-  // Please Provide with Your Podcast embeded Link
-  display: true // Set false to hide this section, defaults to true
-};
-
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
